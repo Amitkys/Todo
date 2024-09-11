02416b3946dc0b6453db4012893d37d43a85f198
KYs <amitkys59@gmail.com>
2024-09-11 10:18:53 +0530

    added component for rending all my todos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import { CreateTodo } from "./components/CreateTodo"
+import { Todos } from "./components/Todos"
 function App(){
   return (
     <div>
       <CreateTodo></CreateTodo>
+      <Todos></Todos>
     </div>
   )
 }
---
+++ frontend/src/components/Todos.jsx
@@ -0,0 +1,10 @@
+export function Todos(){
+    return (
+        <div>
+            <hr />
+            <h1>Go to gym</h1>
+            <h3>bla bla bla</h3>
+            <button>Marks as Done</button>
+        </div>
+    )
+}
