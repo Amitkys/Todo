6fecb2c8c11e110e28cbbd60a87d8370f98b0263
KYs <amitkys59@gmail.com>
2024-09-11 07:57:09 +0530

    changed in component

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,7 +1,7 @@
 export function CreateTodo() {
     return (
         <div>
-            <input type="text" placeholder="Enter Todo" /> <br />
+            <input type="text" placeholder="Write Todo" /> <br />
             <input type="text" placeholder="description"/><br />
             <button>Create Todo</button>
         </div>
